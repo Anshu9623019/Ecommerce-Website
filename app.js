76fc1a1fd00d5ffb3f850d4479446d2b12301575
Anshu9623019 <ak9623019@gmail.com>
2022-06-12 01:52:01 +0530

    first commit

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 //My routes
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const orderRoutes = require("./routes/order");
-const stripeRoutes = require("./routes/stripepayment");
+// const authRoutes = require("./routes/auth");
+// const userRoutes = require("./routes/user");
+// const categoryRoutes = require("./routes/category");
+// const productRoutes = require("./routes/product");
+// const orderRoutes = require("./routes/order");
+// const stripeRoutes = require("./routes/stripepayment");
 
 
 //DB Connection
@@ -33,17 +33,21 @@ app.use(cookieParser());
 app.use(cors());
 
 //My Routes
-app.use("/api", authRoutes);
-app.use("/api",userRoutes);
-app.use("/api",categoryRoutes);
-app.use("/api",productRoutes);
-app.use("/api",productRoutes);
-app.use("/api",orderRoutes);
-app.use("/api",stripeRoutes);
+// app.use("/api", authRoutes);
+// app.use("/api",userRoutes);
+// app.use("/api",categoryRoutes);
+// app.use("/api",productRoutes);
+// app.use("/api",productRoutes);
+// app.use("/api",orderRoutes);
+// app.use("/api",stripeRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
 
+
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static("proj_frontend"));
+}
 //Starting a server
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
